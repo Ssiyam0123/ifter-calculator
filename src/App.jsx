bea1cfb7e035da33082bff7448f7ade2ce63f23d
Ssiyam0123 <ssiyam563@gmail.com>
2025-02-24 17:06:35 +0600

    import tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1 className='text-2xl text-black'>owner nmae</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
